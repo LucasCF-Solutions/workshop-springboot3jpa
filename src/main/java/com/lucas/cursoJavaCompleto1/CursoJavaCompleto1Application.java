9b795652023bff12291ccdd763315e55a7b64bab
LucasCF-Solutions <lucas.caca.cf@gmail.com>
2024-02-23 19:05:36 -0300

    Project created

---
+++ src/main/java/com/lucas/cursoJavaCompleto1/CursoJavaCompleto1Application.java
@@ -0,0 +1,13 @@
+package com.lucas.cursoJavaCompleto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJavaCompleto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJavaCompleto1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/cursoJavaCompleto1/CursoJavaCompleto1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.cursoJavaCompleto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJavaCompleto1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
